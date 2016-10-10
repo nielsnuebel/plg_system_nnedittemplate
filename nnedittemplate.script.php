2504e138c60655c4fd3e31f106d660d3751e7b0f
zero-24 <zero-24@users.noreply.github.com>
2016-10-10 21:35:01 +0200

    initial codecleanup

--- nnedittemplate.script.php
+++ nnedittemplate.script.php
@@ -8,7 +8,7 @@
  */
 
 // No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
 
 /**
@@ -23,31 +23,33 @@ defined('_JEXEC') or die('Restricted access');
  */
 class PlgSystemNNEdittemplateInstallerScript
 {
-    /**
-     * Called before any type of action
-     *
-     * @param  string  $type  type of current action
-     *
-     * @return  boolean  True on success
-     */
-    public function preflight($type)
-    {
-        // make version check only when installing the plugin
-        if ($type != "discover_install" && $type != "install")
-        {
-            return true;
-        }
+	/**
+	 * Called before any type of action
+	 *
+	 * @param   string  $type  type of current action
+	 *
+	 * @return  boolean  True on success
+	 *
+	 * @since   1.0.0
+	 */
+	public function preflight($type)
+	{
+		// make version check only when installing the plugin
+		if ($type != 'discover_install' && $type != 'install')
+		{
+			return true;
+		}
 
-        $version = new JVersion;
+		$version = new JVersion;
 
-        // Abort if the current Joomla release is older
-        if (version_compare($version->getShortVersion(), "3", 'lt'))
-        {
-            Jerror::raiseWarning(null, 'Cannot install NNEdittemplate in a Joomla release prior to 3');
+		// Abort if the current Joomla release is older
+		if (version_compare($version->getShortVersion(), '3', 'lt'))
+		{
+			JError::raiseWarning(null, 'Cannot install NNEdittemplate in a Joomla release prior to 3');
 
-            return false;
-        }
+			return false;
+		}
 
-        return true;
-    }
+		return true;
+	}
 }
